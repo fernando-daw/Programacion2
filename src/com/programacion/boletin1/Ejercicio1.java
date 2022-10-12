be66bbc0240c202297e3f396709b4dbebd602d25
fernando-daw <flagonovoa@danielcastelao.org>
2022-10-12 17:26:50 +0200

    ejercicios boletin 1 de programacion

---
+++ src/com/programacion/boletin1/Ejercicio1.java
@@ -0,0 +1,12 @@
+package com.programacion.boletin1;
+
+public class Ejercicio1 {
+    public static void  main(String[]args){
+
+        int a = 10;
+        int b = 4;
+        int resultado = a * b;
+
+        System.out.println("La altura es " + a + " la base es " + b + " y el area es " + resultado);
+    }
+}
