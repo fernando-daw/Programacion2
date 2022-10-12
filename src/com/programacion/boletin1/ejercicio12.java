bb1ebc66d22f653c856065a0ad4a9bc3a9af0d98
fernando-daw <114103916+fernando-daw@users.noreply.github.com>
2022-10-12 17:55:10 +0200

    Update ejercicio12.java

--- src/com/programacion/boletin1/ejercicio12.java
+++ src/com/programacion/boletin1/ejercicio12.java
@@ -6,10 +6,10 @@ public class ejercicio12 {
     public static void  main(String[]args){
         Scanner permiso=new Scanner(System.in);
         System.out.println("introduce tu edad");
-        int restultado= permiso.nextInt();
+        int resultado= permiso.nextInt();
         int edad=18;
         int carnet=0;
-        carnet=restultado>edad ? restultado: edad;
+        carnet=resultado>edad ? resultado: edad;
         System.out.println( "tienes mas de 18 años? " + resultado);
 
     }
